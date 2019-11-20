6f94eaad005d91390b59be4b0a064cbae5cf9bb2
Kelly McLaughlin <kelly.mclaughlin@joyent.com>
2019-11-19 21:31:33 -0700

    Bump version to 0.2.0
    Update the cueball dependency to v0.2.1 and update the resolver based on the
    Resolver API changes. Also update the cueball-tcp-stream-connection dev
    dependency to v0.1.1.

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@ impl StaticIpResolver {
 }
 
 impl Resolver for StaticIpResolver {
-    fn start(&mut self, s: Sender<BackendMsg>) {
+    fn run(&mut self, s: Sender<BackendMsg>) {
         if !self.started {
             self.backends.iter().for_each(|b| {
                 let backend = Backend::new(&b.0, b.1);
@@ -36,10 +36,5 @@ impl Resolver for StaticIpResolver {
             self.pool_tx = Some(s);
             self.started = true;
         }
-    }
-
-    fn stop(&mut self) {
-        self.started = false;
-        ()
     }
 }
